da519f5050fca613b52fb9c94df5fb108184850f
Fareed-Shah <syedfareed667@gmail.com>
2022-03-15 22:17:56 +0500

    question 1 complet

--- Assignments/Assignment4.dart
+++ Assignments/Assignment4.dart
@@ -1,6 +1,11 @@
 void main() {
 
+// Question : (1)
 
-  
+final List<dynamic> Name = ['Fareed','Fareed','Fareed','Bilal','Bilal','Bilal'];
+ final Dis_Name = Name.toSet();
+print(Dis_Name);
+
+
   
 }
