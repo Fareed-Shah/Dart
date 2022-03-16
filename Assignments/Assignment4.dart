56f8ea360368508841cd5eb9785b209706c7a657
Fareed-Shah <syedfareed667@gmail.com>
2022-03-16 22:17:31 +0500

    task

--- Assignments/Assignment4.dart
+++ Assignments/Assignment4.dart
@@ -1,11 +1,62 @@
+
+
+import 'dart:io';
+
 void main() {
 
-// Question : (1)
+print('Enter Question No :');
+var userinput = stdin.readLineSync();
 
+// Question : (1)
+if(userinput.toString() == '1')
+{
 final List<dynamic> Name = ['Fareed','Fareed','Fareed','Bilal','Bilal','Bilal'];
  final Dis_Name = Name.toSet();
-print(Dis_Name);
+ print('Unique Name from List :  $Dis_Name');
+}
 
+//Question : (2)
+else if(userinput.toString() == '2')
+{
+List numlist = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+List evenlist = [];
+
+
+  for (final i in numlist) {
+    if (i.isEven) {
+      evenlist.add(i);
+      
+    } 
+  }
+  print('Even Numbers from List :  $evenlist');
+  }
+  
+  
+  //Question : (4)
+else if (userinput.toString() == "4") 
+  {
+  var tbl = 7;
+   for(var num=1;num<=15;num++)
+   {
+     print('$tbl * $num = ${tbl*num}');
+   }
+  }
 
   
+    //Question : (4)
+else if (userinput.toString() == "5") 
+  {
+  List fruit = ['apple','banana','mango','strawberry'];
+  
+  for(var item in fruit){
+    
+  print(item);
+
+  }
+  
+
+  }
+
+
+
 }
