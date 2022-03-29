980e801ca802b63ec1da4813b310da8ab8d2c415
Fareed-Shah <syedfareed667@gmail.com>
2022-03-29 21:43:16 +0500

    mArithmeticoperator

---
+++ arithmeticoperator.dart
@@ -0,0 +1,57 @@
+
+import 'dart:io';
+
+void main() {
+
+
+
+  num a =5;
+num b =2;
+num c =5;
+num d = 21/4;
+          // to return value in integes insted of decimal
+num d_int = 21~/4;
+num remain = 2%1;
+          // +Add operator
+print(a+b+c);
+          // -Subtract operator
+print(a-b-c);
+          // Multiply operator
+print(a*b*c);
+          // divide 
+print(d);
+print(d_int);
+          // reminder
+print(remain);
+ 
+
+if(44%2==0)
+{
+ print('Even');
+}
+else{
+  print('odd');
+}
+ 
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
