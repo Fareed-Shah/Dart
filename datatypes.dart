39de348dab600f76c35358bb563228f55a057923
Fareed-Shah <syedfareed667@gmail.com>
2022-03-29 20:53:11 +0500

    number

--- datatypes.dart
+++ datatypes.dart
@@ -2,14 +2,16 @@
 void main() {
 
  
- // Number Datatype.
-
+// Number Datatype.
+// int only take number 
  int age =20;
- double price = 10.45;
+// double take decimal
+ double price = 10.018;
+ // number take both decimal and num
  num rate = 42.0185;
  num rate1 = 42;
-print(rate);
-print(rate1);
+/* print(price);
+print(rate1); */
 
 
 
