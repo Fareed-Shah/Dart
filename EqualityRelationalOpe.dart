d05e55d310786be93f85209afc0e37b14b90323b
Fareed-Shah <syedfareed667@gmail.com>
2022-03-29 21:58:07 +0500

    ERO

--- EqualityRelationalOpe.dart
+++ EqualityRelationalOpe.dart
@@ -2,18 +2,33 @@ void main() {
   
 num a =10;
 num b = 12;
+num f = 7;
+num e=22;
 bool c = a==b;
 bool d = a!=b;
-print(a>b);
+/*  print(a>b);
 print(a<b);
 print(c);
-print(d);
+print(d); */
 
 
+          // && Retrun true if all condition's are true
+if(a==b && f!=e)
+{
+  print('Condition True');
+}
+else{
+  print('Condition Return False');
+}
 
-
-
-
+        // && Retrun true if all condition's are true
+if(a==b || f!=e)
+{
+  print('Condition True');
+}
+else{
+  print('Condition Retur False');
+}
 
 
 
