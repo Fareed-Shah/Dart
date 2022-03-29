f925991d99cd87479ccf03f440dae4cd740b8d39
Fareed-Shah <syedfareed667@gmail.com>
2022-03-29 21:15:57 +0500

    datatypes

--- datatypes.dart
+++ datatypes.dart
@@ -10,22 +10,23 @@ void main() {
  // number take both decimal and num
  num rate = 42.0185;
  num rate1 = 42;
-/* print(price);
-print(rate1); */
+ print(price);
+print(rate1); 
 
                   // String
 
 String name ="Fareed Shah";
 String Address = "Block B Wahid Colony";
-// concatenate string and int 
-// you need to convert other data type to string using to.string() to concatenate
-//with string
+        // concatenate string and int 
+        // you need to convert other data type to string using to.string() to concatenate
+        //with string
+
 print(name + " "+ rate.toString());
 
  // Bool
  bool calc;
  calc = 12 < 5 ;
- //print(calc);
+ print(calc);
  
  bool str  ;
 var a = "ac";
