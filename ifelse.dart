e7c6d9b340fd5f761e5394871c3013b222c372a5
Fareed-Shah <syedfareed667@gmail.com>
2022-03-29 22:17:12 +0500

    Nested if

--- ifelse.dart
+++ ifelse.dart
@@ -1,3 +1,44 @@
+
+
+
 void main() {
-  print('dart');
+  
+  
+  
+bool offeredwinter = false;
+bool offeredsummer = false;
+
+
+if (offeredwinter) {
+
+  if (offeredsummer) {
+    print('course offered in both winter and summe');
+   }
+   else{
+     print('course only offer in winter');
+   }
+ 
+}
+else if(offeredsummer){
+  print('Course only offer in Summer');
+}
+else{
+  print('No Course in this Summer or winter');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
 }
