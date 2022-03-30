641d414e6ffe231d7410d6e1518806f2d6a0b04d
Fareed-Shah <syedfareed667@gmail.com>
2022-03-30 21:49:37 +0500

    lst

--- List.dart
+++ List.dart
@@ -5,11 +5,14 @@ void main() {
 
 
 List lst = [8,2,5,11,9];
-print(lst.first);
+/* print(lst.first);
 print(lst.last);
 print(lst.elementAt(1));
 print(lst.length);
- 
+  */
 
+
+lst.replaceRange(0,3,[7,4,6]);
+print(lst);
   
 }
