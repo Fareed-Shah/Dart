7f6d31e7d151b32fc6c3059f902846e386f5f2bd
Fareed-Shah <syedfareed667@gmail.com>
2022-03-30 22:50:40 +0500

    list

--- List.dart
+++ List.dart
@@ -5,31 +5,31 @@ void main() {
 
 
 List lst = [50,80,10,22,12,36,20,30];
-/* List lst1 = [60,80,20,30];
+ List lst1 = [60,80,20,30];
 List lst3 = lst + lst1;
 
-//var rs = lst.reduce((value, element) => value+element);
+var rs = lst.reduce((value, element) => value+element);
 var rs1 = lst3.reduce((value, element) => value+element);
-print('Sum of two list : $rs1'); */
+print('Sum of two list : $rs1'); 
 
-      // get range for selection of multiple index
-//print(lst.getRange(3,lst.length ).toList());
+                // get range for selection of multiple index
+print(lst.getRange(3,lst.length ).toList());
 lst.replaceRange(0, 5, [1,2,3,4,5]);
 print(lst);
-//print(lst.reversed.toList()[0]);
-/*  print(lst.remove(7));
+print(lst.reversed.toList()[0]);
+  print(lst.remove(7));
 lst.removeAt(1);
 lst.removeAt(lst.length-1); 
-lst.removeRange(0,5); */
-//lst.clear();
-//lst.add('Fareed');
-//lst.addAll(['Fareed','Shah',1]);
-//lst.removeLast();
-//lst.contains(5);
-//print(lst.contains(5));
+lst.removeRange(0,5); 
+lst.clear();
+lst.add('Fareed');
+lst.addAll(['Fareed','Shah',1]);
+lst.removeLast();
+lst.contains(5);
+print(lst.contains(5));
 
 
-/* bool check = lst.contains('Fareed');
+ bool check = lst.contains('Fareed');
 
 if(check)
 {
@@ -38,7 +38,7 @@ if(check)
 else
 {
   print('not in list');
-} */
+} 
 
 /* List lst = [8,2,5,11,9];
 /* print(lst.first);
