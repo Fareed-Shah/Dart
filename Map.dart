32b89d095cd9adf16066487b1affe00b268cd3f1
Fareed-Shah <syedfareed667@gmail.com>
2022-03-31 23:26:37 +0500

    map

--- Map.dart
+++ Map.dart
@@ -4,23 +4,8 @@ void main() {
 
 //  Dart Map Using Literal's
 
-Map<dynamic,dynamic> Emp = {
-  'Name':'Fareed',
-  'Age':20,
-  'Address':'Block B North Nazimabad',
-  'Salary':25410.12 ,
-  };
+Map Info = {'Name':'Fareed'};
+print(Info.keys);
+print(Info.values);
 
-  
-  Emp.addAll({'Skills':['SQL','Dart','Flutter','Python']});
-    print(Emp);
- 
- // Dart Map Using Constructor
-  
-  Map emp = new Map();
-  emp['Emp_Name'] = 'Fareed Shah';
-  emp['Emp_Email'] = 'syedfareed667@gmail.com';
-   print(emp);
-  emp.forEach(((key, value) => print('$key:$value')));  
- 
 }
