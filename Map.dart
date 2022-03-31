f9d9f4bbfe29027d4ad4814af635c9834cc63f2c
Fareed-Shah <syedfareed667@gmail.com>
2022-03-31 23:31:42 +0500

    map

--- Map.dart
+++ Map.dart
@@ -4,8 +4,12 @@ void main() {
 
 //  Dart Map Using Literal's
 
-Map Info = {'Name':'Fareed'};
+Map Info = {'Name':'Fareed','Mobile':'0316-2511883'};
 print(Info.keys);
 print(Info.values);
+print(Info);
+print(Info.length);
+print(Info.containsKey('Name'));
+
 
 }
