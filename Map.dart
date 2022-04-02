1d080368cecc8eee10b0315334a312423c70e8a8
Fareed-Shah <syedfareed667@gmail.com>
2022-04-02 15:29:00 +0500

    f

--- Map.dart
+++ Map.dart
@@ -5,6 +5,7 @@ void main() {
 //  Dart Map Using Literal's
 
 Map Info = {'Name':'Fareed','Mobile':'0316-2511883'};
+Map Info1 = {'Name':'Naveed','Mobile':'0315-2511883'};
 /* print(Info.keys);
 print(Info.values);
 print(Info);
@@ -15,8 +16,13 @@ Info.remove('Mobile');
 Info['Name'] = 'Ahmed';
 Info.addAll({'Name':'Fareed'});
 print(Info.containsKey('Name'));
- */
 Info.removeWhere((key, value) => value.startsWith('F'));
-print(Info);
+ */
 
+  var f1 = {1: 'Apple', 2: 'Orange'};
+  var f2 = {3: 'Banana'};
+  var f3 = {4: 'Mango'};
+  var fruit = {}..addAll(f1)..addAll(f2)..addAll(f3);
+    print(fruit);
+  
 }
