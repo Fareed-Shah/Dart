83a372aca896b18ab24f096b997a63fa85026757
Fareed-Shah <syedfareed667@gmail.com>
2022-04-09 14:51:16 +0500

    map

--- Map.dart
+++ Map.dart
@@ -5,9 +5,12 @@ void main() {
 
 
 Map Name_Mobile = {'Name':['Fareed','Naveed','Ali'],'Mobile':['0321-25863','090078','218408']};
-print(Name_Mobile['Name'][0]);
+Map City_Address = {'City':['Karachi','Lahore','Islamabad'],'address':['Block B','Hyderi','F-10']};
+Map cmbine = {}..addAll(Name_Mobile)..addAll(City_Address);
+print(cmbine.keys);
 
 
+//print(Name_Mobile['Name'][0]);
 //  Dart Map Using Literal's
 /* 
 Map Name_Mobile = {'Name':'Fareed','Mobile':'0316-2511883','Telephone':'090078601'};
