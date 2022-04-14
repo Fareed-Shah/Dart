30c63be089e4d22dbeea8f0977a1a6ce3d928f78
Fareed-Shah <syedfareed667@gmail.com>
2022-04-14 22:59:49 +0500

    loop

--- function.dart
+++ function.dart
@@ -2,25 +2,31 @@
 void main() {
    
 
-var std1 =studentPerc(7845, 85,77, 72);
-
+/* var std1 =studentPerc(7845, 85,77, 72); */
+int RollNo;
+Map Studentlist = {'RollNo':[147,258,369,987,456,321],
+                  'Name':['Fareed','Ali','Bilal','Owais','Naveed','Osama']};
 
 
 
 }
 
 
-studentPerc(int RollNo,int maths,int urdu,int eng){
+
+
+
+/* studentPerc(int RollNo,int maths,int urdu,int eng){
 int obt = maths+urdu+eng;
 double per = (obt*100)/300;
 print('$RollNo Percantage is: $per');
 return RollNo.toString()  +' '+   per.toString();
 }
 
-/* studentData(int RollNo,String name){
+studentData(int RollNo,String name){
 
 Map Studentlist = {'RollNo':[147,258,369,987,456,321],
                   'Name':['Fareed','Ali','Bilal','Owais','Naveed','Osama']};
 
+ Studentlist.containsValue(RollNo);
 } */
 
