2e69a121b53f32eaa64a793b326eccad7f5b77d1
Fareed-Shah <syedfareed667@gmail.com>
2022-04-14 22:00:58 +0500

    loop

--- forloop.dart
+++ forloop.dart
@@ -1,7 +1,7 @@
 void main() {
 
 
-var name = ['Fareed','ali','bilal'];
+/* var name = ['Fareed','ali','bilal'];
 
 /* for (var item in name) {
   print(item);
@@ -10,15 +10,19 @@ var name = ['Fareed','ali','bilal'];
 for (var i = 0; i < name.length; i++) {
   print(name[i]);
 }
+ */
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89];
+  Set<int> c = {};
 
-
-
-
-
-
-
-
-
+  for (var i in a) {
+    for (var j in b) {
+      if (i == j) {
+        c.add(i);
+      }
+    }
+  }
+  print(c.toList());
 
 
 
