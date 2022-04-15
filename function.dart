a3797b9943abcd3b6dfcd563601a083063a53d69
Fareed-Shah <syedfareed667@gmail.com>
2022-04-15 22:32:09 +0500

    map

--- function.dart
+++ function.dart
@@ -4,8 +4,18 @@ void main() {
 
 /* var std1 =studentPerc(7845, 85,77, 72); */
 
+/* int RollNo;
+Map Studentlist = {'RollNo':[147,258,369,987,456,321],
+                  'Name':['Fareed','Ali','Bilal','Owais','Naveed','Osama']};
 
+ List a  =Studentlist.values.toList()[0];
 
+ print(a); */
+
+Map RollNo = {'RollNo':[147,258,369,987,456,321]};
+Map Names = {'Name':['Fareed','Ali','Bilal','Owais','Naveed','Osama']};
+Map combine = {}..addAll(RollNo)..addAll(Names);
+print(combine.values.toList());
 
 }
 
